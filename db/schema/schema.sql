c360ca5722cb0439108bd3164788fdf5291dc87e
Sebastian Lynch <lynchseb@gmail.com>
2020-03-06 19:50:04 +0000

    create sql schema, create menu seed data

---
+++ db/schema/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE menu (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  food_category VARCHAR(255) NOT NULL,
+
+  item_img TEXT NOT NULL,
+
+  price SMALLINT NOT NULL,
+);
+
+CREATE TABLE orders (
+  user_id INTEGER REFERENCES users.id ON DELETE CASCADE,
+  order_id INTEGER REFERENCES menu.id ON DELETE CASCADE,
+
+  order_status BOOLEAN DEFAULT FALSE,
+  order_begin TIMESTAMP,
+  order_end TIMESTAMP,
+  order_date DATE NOT NULL,
+);
---
+++ db/seeds/menu_seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO menu (name, description, food_category, item_img, price) VALUES
+('Fish -N- Chips', "The best fried halibut you'll ever have. Made to oder by S.Gamgee, you can't say no to that!", 'fish', 'fish-and-chips.jpg', 1999),
+('Beef Wellington', "Beef fillet, surrounded in love and baked to perfection in Fatty Bolger approved puff pastry. This'll be sure to put a smile on your Old Gaffers face", 'beef', 'image', 2999);

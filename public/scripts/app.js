6b4216e72b39e0b033b9613277b3fcf8c1dbdf4d
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-03-06 19:45:43 +0000

    the base of html and css works with the nav bar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,8 +3,33 @@ $(() => {
     method: "GET",
     url: "/api/users"
   }).done((users) => {
-    for(user of users) {
+    for (user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+});
+// for nav
+$(() => {
+  const indicator = document.querySelector('.nav-indicator');
+  const items = document.querySelectorAll('.nav-item');
+
+  function handleIndicator(el) {
+    items.forEach(item => {
+      item.classList.remove('is-active');
+      item.removeAttribute('style');
+    });
+
+    indicator.style.width = `${el.offsetWidth}px`;
+    indicator.style.left = `${el.offsetLeft}px`;
+    indicator.style.backgroundColor = el.getAttribute('active-color');
+
+    el.classList.add('is-active');
+    el.style.color = el.getAttribute('active-color');
+  }
+
+
+  items.forEach((item, index) => {
+    item.addEventListener('click', (e) => { handleIndicator(e.target) });
+    item.classList.contains('is-active') && handleIndicator(item);
+  });
 });

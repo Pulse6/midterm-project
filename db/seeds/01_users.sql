c140bab6b30292047a4f6638797c59d2b32ee52a
Sebastian Lynch <lynchseb@gmail.com>
2020-03-06 19:54:41 +0000

    commented 01_users.sql, 05_widgets.sql from schema and seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- -- Users table seeds here (Example)
+-- INSERT INTO users (name) VALUES ('Alice');
+-- INSERT INTO users (name) VALUES ('Kira');

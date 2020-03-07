2979d0684d31d587eab5c29e8350cbbc47a4868d
Shaheer Ahmed <shaheerahmeddev@gmail.com>
2020-03-07 03:07:36 +0000

    add dollar sign to price, divide price by 100

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,7 +18,7 @@ $(() => {
       const wrapper = $("<div class='menu-item'>")
         .append($("<h2>")
           .text(food.name + " - ")
-          .append($("<span class='price'>").text(food.price)))
+          .append($("<span class='price'>").text(`$${food.price/100}`)))
         .append($("<p>").text(food.description));
 
       $(".menu").append(wrapper);

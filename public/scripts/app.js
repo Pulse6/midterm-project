57a194391d1de5601afce4c1f72c039d4640b5fb
Shaheer Ahmed <shaheerahmeddev@gmail.com>
2020-03-07 03:10:10 +0000

    move nav bar to top of file, add comments by code functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,4 @@
-$(() => {
-  // console.log("it works")
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for (user of users.users) {
-      $("<div>").text(user.name).appendTo($(""));
-    }
-  });
-
-  $.ajax({
-    method: "GET",
-    url: "/api/menu"
-  }).done((res) => {
-    console.log(res)
-    for (food of res.menu) {
-      const wrapper = $("<div class='menu-item'>")
-        .append($("<h2>")
-          .text(food.name + " - ")
-          .append($("<span class='price'>").text(`$${food.price/100}`)))
-        .append($("<p>").text(food.description));
-
-      $(".menu").append(wrapper);
-      // $("<h2>").text(food.name + " - ").appendTo($(".menu"));
-      // $("<span>").text(" " + food.price).appendTo($(".menu"));
-      // $("<p>").text(food.description).appendTo($(".menu"));
-    }
-  });
-
-});
-// for nav
+/* ———————————————————— NAVBAR ———————————————————— */
 $(() => {
   const indicator = document.querySelector('.nav-indicator');
   const items = document.querySelectorAll('.nav-item');
@@ -53,4 +22,38 @@ $(() => {
     item.addEventListener('click', (e) => { handleIndicator(e.target) });
     item.classList.contains('is-active') && handleIndicator(item);
   });
+});
+
+/* ———————————————————— GET USERS FROM SQL ———————————————————— */
+$(() => {
+  // console.log("it works")
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done((users) => {
+    for (user of users.users) {
+      $("<div>").text(user.name).appendTo($(""));
+    }
+  });
+
+/* ———————————————————— GET MENU FROM SQL ———————————————————— */
+  $.ajax({
+    method: "GET",
+    url: "/api/menu"
+  }).done((res) => {
+    console.log(res)
+    for (food of res.menu) {
+      const wrapper = $("<div class='menu-item'>")
+        .append($("<h2>")
+          .text(food.name + " - ")
+          .append($("<span class='price'>").text(`$${food.price/100}`)))
+        .append($("<p>").text(food.description));
+
+      $(".menu").append(wrapper);
+      // $("<h2>").text(food.name + " - ").appendTo($(".menu"));
+      // $("<span>").text(" " + food.price).appendTo($(".menu"));
+      // $("<p>").text(food.description).appendTo($(".menu"));
+    }
+  });
+
 });

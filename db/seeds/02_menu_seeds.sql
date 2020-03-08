8ab234932aea71fc066727ebb0d2f17725c0cf37
Shaheer Ahmed <shaheerahmeddev@gmail.com>
2020-03-08 03:25:24 +0000

    create order_seeds.sql, put all schemas in one folder

--- db/seeds/02_menu_seeds.sql
+++ db/seeds/02_menu_seeds.sql
@@ -2,17 +2,21 @@ INSERT INTO menu (name, description, food_category, item_img, price) VALUES
   (
     'Fish -N- Chips',
     'The best fried halibut you''ll ever have. Made to oder by S.Gamgee, you can''t say no to that!',
-    'fish',
+    'entrees',
     'fish-and-chips.jpg',
     1999
   ),
   (
     'Beef Wellington',
     'Beef fillet, surrounded in love and baked to perfection in Fatty Bolger approved puff pastry. This''ll be sure to put a smile on your Old Gaffers face',
-    'beef',
+    'entrees',
     'image',
     2999
+  ),
+  (
+    'Chicken Nuggets',
+    'The best nuggets around town',
+    'entrees',
+    'image',
+    599
   );
-
-INSERT INTO orders (user_id, menu_id, item_name, amount)
-VALUES (1, 1, 'Fish -N- Chips', 1);

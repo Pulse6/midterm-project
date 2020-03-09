f1bb7b352d9b82bf7be480bb539e4fa762b19e8b
Sebastian Lynch <lynchseb@gmail.com>
2020-03-09 16:36:47 +0000

    fix img rendering. upload images for remainder of menu

--- db/seeds/02_menu_seeds.sql
+++ db/seeds/02_menu_seeds.sql
@@ -3,14 +3,14 @@ INSERT INTO menu (name, description, food_category, item_img, price) VALUES
     'Fish -N- Chips',
     'The best fried halibut you''ll ever have. Made to order daily by S.Gamgee himself, you can''t say no to that!',
     'entrees',
-    'public/images/fish-and-chips.jpg',
+    '/images/fish-and-chips.jpg',
     1999
   ),
   (
     'Beef Wellington',
     'Beef fillet, surrounded in love and baked to perfection in Fatty Bolger approved puff pastry. This''ll be sure to put a smile on your Old Gaffers face.',
     'entrees',
-    'public/images/BeefWellington.jpg',
+    '/images/BeefWellington.jpg',
     2999
 
   ),
@@ -18,64 +18,64 @@ INSERT INTO menu (name, description, food_category, item_img, price) VALUES
     'Shepherd''s pie',
     'From the bounty of the fields we''ve all ''borrowed'' from, but this time with his approval: Farmer Maggot Shepherd''s pie will not dissapoint.',
     'entrees',
-    'spheherd pie',
+    '/images/shepherds-pie.jpg',
     1499
   ),
   (
     'Chef''s Curry',
     'While we Hobbits don''t normally hold well with foreign food this spicy curry from the East came to us back when Old Bilbo turned up not dead. Now we still don''t reckon much for travelling see, but this here is a mighty fine dish that many a Gaffer has taken too and you should too!',
     'entrees',
-    'curry',
+    '/images/rustic-curry.jpg',
     1499
   ),
   (
     'The Prime Rib',
     'From the pastures of Tookland comes a feast that is every Hobbit''s favourite meal. Prime rib served with creamy mashed potatoes, mixed vegetables, gravy and Shire puddings.',
     'entrees',
-    'prime rib',
+    '/images/prime-rib.jpeg',
     2499
   ),
   (
     'Sauteed Mushrooms',
     'A far safer shortcut to mushrooms than you''re used to and our number #1 bestseller. A collection of wild mushrooms and mushrooms brough to us by Farmer Maggot this dish speaks for itself.',
     'appetizers',
-    'mushrooms',
-    9999
+    '/images/mushrooms.jpg',
+    999
   ),
   (
     'Fresh Chips',
     'Brought to you by our guest chef S.Gamgee these are the best damned chips you''ll ever have.',
     'appetizers',
-    'chips',
-    4999
+    '/images/french-fries.jpeg',
+    499
   ),
   (
     'French Onion Soup',
-    'We''re not sure why or what French is but this onion soup''s name is as old as our love for it. With our broth includin vintages from the wine-cellars of Brandybuck Hall this soup has quickly become a local favourite',
+    'We''re not sure where French comes from or what exactly that is but this onion soup''s name is as old as our love for it. With our broth including vintages from the wine-cellars of Brandybuck Hall this soup has quickly become a local favourite',
     'appetizers',
-    'french onion soup',
-    9999
+    '/images/french-onion-soup.jpg',
+    999
   ),
   (
     'Chocolate gateau',
     'Dark, decadent and as light as you can imagine. Some say Hobbits are the best bakers this side of the Brandywine, but they''d be wrong...we''re the best bakers in Middle Earth',
     'desserts',
-    'chocolate cake img',
-    7999
+    '/images/chocolate-gateau.jpeg',
+    799
   ),
   (
     'Strawberries and Cream',
     'From the orchards and dairies of the Cotton''s this dish is sweet, light and as lovely as Rossie Cotton herself',
     'desserts',
-    'strawberries img',
-    5999
+    '/images/strawberries-cream.jpg',
+    599
   ),
   (
     'Butter Tarts',
     'These are the best tarts around. Enough said',
     'desserts',
-    'butter tarts',
-    5999
+    '/images/butter-tarts.jpg',
+    599
   );
 
 
--- db/seeds/04_drink_seeds.sql
+++ db/seeds/04_drink_seeds.sql
@@ -4,26 +4,26 @@ INSERT INTO drinks (name, description, food_category, item_img, price) VALUES
     'Smooth and creamy this Stoute is the pride of the Brandybucks and we''re proud to serve it to you',
     'alcoholic',
     'stoute img',
-    5999
+    599
   ),
   (
     'Hobbit''s Ale',
     'The most passable ale you''ll find. Goes down easy, is light on the pocket and there''s always plenty more',
     'alcoholic',
     'ale image',
-    3999
+    399
   ),
   (
     'Second Breakfast Sour',
     'Of our own design and exclusive to Second Breakfast, you won''t sour on this twist on our own Hobbit''s Ale',
     'alcoholic',
     'sour ale',
-    4999
+    499
   ),
   (
     'Juices',
     'A variety of locally sourced beverages suitable for your younglings',
     'non-alcoholic',
     'juice',
-    2999
+    299
   );

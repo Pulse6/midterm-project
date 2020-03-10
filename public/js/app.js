be8f8140865a3eb9a9be027458e431b275e65f81
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-03-10 15:53:54 +0000

    clean up more code made logo bigger and change background color

--- public/js/app.js
+++ public/js/app.js
@@ -1,97 +1,19 @@
-/* ———————————————————— GET USERS FROM SQL ———————————————————— */
-// $(() => {
-//   // console.log("it works")
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for (user of users.users) {
-//       $("<div>").text(user.name).appendTo($(""));
-//     }
-//   });
+// for adding item in cart
+$("body").on("click", ".add-to-order-top-container", function (event) {
+  const item = $(event.target);
+  const food = item.parent().parent().parent().parent()
+  const name = food.find(".gallery-title").text()
+  const price = parseInt(food.find(".add-to-order-price").text().slice(1) * 100) / 100
+  const quantity = $(".shopping-cart-items li:contains(" + name + ")").find(".item-count").text()
+  const item_count = $(".shopping-cart-items li:contains(" + name + ")")
+  const pic = food.find(".img-fluid").attr('src')
 
-// const addItemHandlers = () => {
-//   $(".add-to-cart").click(function (event) {
-//     const item = $(event.target);
-//     const id = item.attr('data-food-id');
-// console.log()
-//     const food = item.parent().parent()
-//     const name = food.find(".menu-name").text()
-//     const price = food.find(".menu-price").text()
-// const amount = item.parent().parent().parent().parent()
-// console.log(amount)
-// const cart = $(".item-template")
-// console.log(cart)
-// for (let item of cart) {
-//   console.log(item.attr('data-id'))
-// }
-// console.log(cart)
-//     if ($(".item-template").find(".item-name").text() === name) {
-//       console.log("already there")
-
-//     } else {
-//       $(".item-template").append(`
-//       <div data-id="${id}">
-//       <li class="item-name" >${name}</li>
-//       <li class="item-price">${price}</li>
-//       <li class="item-count">
-//       <input class="item-count" type="number" value="1">
-//       <button class="remove-item">REMOVE</button>
-//       </li>
-//       </div>
-//       `)
-//     }
-// for (item of cart) {
-//   const foodID = item.attr('data-id')
-//   if (foodID === id) {
-//     $(".item-count").val() + 1
-//   }
-// }
-// for ($(.item-template))
-// });
-// $(".plus-item").click(function (event) {
-//   const item = $(event.target);
-//   const id = item.parent().attr('data-food-id');
-
-//   console.log(`add count to id: ${id}`)
-//   const count = $(`[data-food-id="${id}"] .count-for-item`)
-//   let getNowVal = count.val() + 1
-//   count.val(getNowVal)
-//   count.text(getNowVal)
-// });
-
-// $(".subtract-item").click(function () {
-//   const item = $(event.target);
-//   const id = item.parent().attr('data-food-id');
-
-//   // console.log(`add count to id: ${id}`)
-//   const count = $(`[data-food-id="${id}"] .count-for-item`)
-//   let getNowVal = count.val() - 1
-//   count.val(getNowVal)
-//   count.text(getNowVal)
-//   if (getNowVal <= 0) {
-//     count.val(0)
-//     count.text(0)
-//   }
-// });
-// }
-const addItemHandlers = () => {
-  $(".add-to-order-top-container").click(function (event) {
-    const item = $(event.target);
-    const food = item.parent().parent().parent().parent()
-    const name = food.find(".gallery-title").text()
-    const price = parseInt(food.find(".add-to-order-price").text().slice(1) * 100) / 100
-    const quantity = $(".shopping-cart-items li:contains(" + name + ")").find(".item-count").text()
-    const item_count = $(".shopping-cart-items li:contains(" + name + ")")
-    const pic = food.find(".img-fluid").attr('src')
-
-    if (item_count.length != 0) {
-      const el = parseInt($(".shopping-cart-items li:contains(" + name + ")").find(".item-quantity").find(".item-count").text());
-      // console.log(el)
-      $(".shopping-cart-items li:contains(" + name + ")").find(".item-quantity").find(".item-count").text(el + 1);
-    }
-    else {
-      $(".shopping-cart-items").append(`
+  if (item_count.length != 0) {
+    const el = parseInt($(".shopping-cart-items li:contains(" + name + ")").find(".item-quantity").find(".item-count").text());
+    $(".shopping-cart-items li:contains(" + name + ")").find(".item-quantity").find(".item-count").text(el + 1);
+  }
+  else {
+    $(".shopping-cart-items").append(`
       <li class="clearfix">
         <img src="${pic}" alt="item1" />
         <span class="item-name">${name}</span>
@@ -100,12 +22,11 @@ const addItemHandlers = () => {
         <span class="item-remove">X</span>
       </li>
     `);
-    // removeItemFromCart()
 
   }
-  });
-}
+});
 
+// for adding all menu item
 const addEntree = (entree) => {
   const wrapper = `
   <div class="gallery-page-${entree.food_category} gallery-page">
@@ -134,60 +55,125 @@ const addEntree = (entree) => {
   $(".gallery").append(wrapper);
 }
 
-const addToTotalOrders = () => {
-  $('.add-to-order-top-container').click(function () {
-    $(".shopping-cart").fadeIn("fast");
-    const fullList = $(".shopping-cart-items")
-    // console.log(fullList)
-    let totalOrders = parseInt($('.badge-outer').text());
-    // console.log(totalOrders)
-    totalOrders += 1;
-    $('.badge-outer').html(totalOrders);
-    $('.badge-inner').html(totalOrders);
+// for updating counter for number of orders
+$("body").on("click", ".add-to-order-top-container", function () {
+  $(".shopping-cart").fadeIn("fast");
+  // const fullList = $(".shopping-cart-items")
+  let totalOrders = parseInt($('.badge-outer').text());
+  totalOrders += 1;
+  $('.badge-outer').html(totalOrders);
+  $('.badge-inner').html(totalOrders);
+})
+
+// for update total price in cart
+$("body").on("click", ".add-to-order-top-container", function (event) {
+  const item = $(event.target);
+  const food = item.parent().parent().parent().parent()
+  const price = parseInt(food.find(".add-to-order-price").text().slice(1) * 100)
+  let orderTotal = (($(".main-color-text").text().slice(1) * 100) + price)
+  orderTotal = Math.round(orderTotal) / 100
+
+  $(".main-color-text").text("$" + orderTotal)
+})
+
+// for when user click on remove button in cart
+$("body").on("click", ".item-remove", function () {
+  // const item = $(event.target)
+  const target = $(this).parent()
+  const price = parseInt(target.find(".item-price").text().slice(1) * 100)
+  const amount = parseInt(target.find(".item-quantity").find(".item-count").text())
+  const currentTot = parseInt($(".main-color-text").text().slice(1) * 100)
+  const newtot = (currentTot - (amount * price)) / 100
+
+  $(".main-color-text").text("$" + newtot)
+  let totalOrders = parseInt($('.badge-outer').text());
+  totalOrders -= amount;
+  $('.badge-outer').html(totalOrders);
+  $('.badge-inner').html(totalOrders);
+
+  $(this).parent().remove()
+})
+
+// for filter Categories
+$("body").on("click", ".paging-link", function (event) {
+  event.preventDefault();
+  const page = $(this).text().toLowerCase();
+
+  $('.gallery-page').addClass('hidden');
+  $('.gallery-page-' + page).removeClass('hidden').fadeTo("slow");
+  $('.paging-link').removeClass('active');
+  $(this).addClass("active");
+});
+
+// for adding all item back to menu
+$("body").on("click", ".menu", function (event) {
+  event.preventDefault();
+  $('.gallery-page').removeClass('hidden').fadeTo("slow");
+  $(this).addClass("active");
+});
+////////////////////////////////////////////////////////////////////////
+
+// for hidding cart at first
+function defaultHideCart() {
+  $('.shopping-cart').hide();
+}
+
+// for showing cart when user add in item
+function showCartOnToggle() {
+  $('#cart').click(function (event) {
+    event.preventDefault();
+    $(".shopping-cart").fadeToggle("fast");
   })
 }
 
-const addToOrderTotal = () => {
-  $('.add-to-order-top-container').click(function (event) {
-    const item = $(event.target);
-    const food = item.parent().parent().parent().parent()
-    // const name = food.find(".gallery-title").text()
-    const price = parseInt(food.find(".add-to-order-price").text().slice(1) * 100)
-    // console.log(price)
-    let orderTotal = (($(".main-color-text").text().slice(1) * 100) + price) / 100
-    // console.log(orderTotal)
-    $(".main-color-text").text("$" + orderTotal)
-  })
+/* function showAddToOrderButtonOnHover() {
+  Handle add to order on hover
+  $('.add-to-order-container').addClass('hidden');
+  $('.gallery-item').hover(
+    function() {
+      $('.add-to-order-container').removeClass('hidden');
+    },
+    function() {
+      $('add-to-order-container').addClass('hidden');
+    }
+  );
+} */
+
+function getMenu() {
+  $.ajax({
+    method: "GET",
+    url: "/api/menu"
+  }).done((res) => {
+    localStorage.setItem('food', JSON.stringify(res.menu));
+    for (food of res.menu) {
+      addEntree(food);
+    }
+  });
 }
-// $(function () {
-  // console.log("it works!!")
-// const removeItemFromCart = () => {
-  $("body").on("click", ".item-remove", function (event) {
-    const item = $(event.target)
-    // console.log(target.length)
-    const target = $(this).parent()
-    // const dammmm = target.attr('data-name')
-    // console.log(dammmm)
-    // const id = item.attr('data-food-id')
-    // console.log($(this).parent())
-    const price = parseInt(target.find(".item-price").text().slice(1) * 100)
-    // console.log("price " + price)
-    const amount = parseInt(target.find(".item-quantity").find(".item-count").text())
-    // console.log("amount "+amount)
-    const currentTot = parseInt($(".main-color-text").text().slice(1) * 100)
-    // console.log("current tot "+currentTot)
-    const newtot = (currentTot - (amount * price)) / 100
-    // console.log("newtot  "+newtot)
-    $(".main-color-text").text("$" + newtot)
 
-    let totalOrders = parseInt($('.badge-outer').text());
-    // console.log(totalOrders)
-    totalOrders -= amount;
-    $('.badge-outer').html(totalOrders);
-    $('.badge-inner').html(totalOrders);
+function getDrinks() {
+  $.ajax({
+    method: "GET",
+    url: "/api/drinks"
+  }).done((res) => {
+    localStorage.setItem('drinks', JSON.stringify(res.drinks))
+    for (drink of res.drinks) {
+      addEntree(drink);
+    }
+  });
+}
 
-    $(this).parent().remove()
-  })
-// }
-// });
-
+$(function () {
+  getMenu();
+  getDrinks();
+  $.ajax({
+    method: "GET",
+    url: "/api/menu"
+  }).done((res) => {
+    for (food of res.menu) {
+      addEntree(food);
+    }
+  });
+  defaultHideCart();
+  showCartOnToggle();
+});
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,80 +1,3 @@
-/* ———————————————————— GET USERS FROM SQL ———————————————————— */
-// $(() => {
-//   // console.log("it works")
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for (user of users.users) {
-//       $("<div>").text(user.name).appendTo($(""));
-//     }
-//   });
-
-// const addItemHandlers = () => {
-//   $(".add-to-cart").click(function (event) {
-//     const item = $(event.target);
-//     const id = item.attr('data-food-id');
-// console.log()
-//     const food = item.parent().parent()
-//     const name = food.find(".menu-name").text()
-//     const price = food.find(".menu-price").text()
-// const amount = item.parent().parent().parent().parent()
-// console.log(amount)
-// const cart = $(".item-template")
-// console.log(cart)
-// for (let item of cart) {
-//   console.log(item.attr('data-id'))
-// }
-// console.log(cart)
-//     if ($(".item-template").find(".item-name").text() === name) {
-//       console.log("already there")
-
-//     } else {
-//       $(".item-template").append(`
-//       <div data-id="${id}">
-//       <li class="item-name" >${name}</li>
-//       <li class="item-price">${price}</li>
-//       <li class="item-count">
-//       <input class="item-count" type="number" value="1">
-//       <button class="remove-item">REMOVE</button>
-//       </li>
-//       </div>
-//       `)
-//     }
-// for (item of cart) {
-//   const foodID = item.attr('data-id')
-//   if (foodID === id) {
-//     $(".item-count").val() + 1
-//   }
-// }
-// for ($(.item-template))
-// });
-// $(".plus-item").click(function (event) {
-//   const item = $(event.target);
-//   const id = item.parent().attr('data-food-id');
-
-//   console.log(`add count to id: ${id}`)
-//   const count = $(`[data-food-id="${id}"] .count-for-item`)
-//   let getNowVal = count.val() + 1
-//   count.val(getNowVal)
-//   count.text(getNowVal)
-// });
-
-// $(".subtract-item").click(function () {
-//   const item = $(event.target);
-//   const id = item.parent().attr('data-food-id');
-
-//   // console.log(`add count to id: ${id}`)
-//   const count = $(`[data-food-id="${id}"] .count-for-item`)
-//   let getNowVal = count.val() - 1
-//   count.val(getNowVal)
-//   count.text(getNowVal)
-//   if (getNowVal <= 0) {
-//     count.val(0)
-//     count.text(0)
-//   }
-// });
-// }
 const addItemHandlers = () => {
   $(".add-to-order-top-container").click(function (event) {
     const item = $(event.target);
@@ -87,7 +10,6 @@ const addItemHandlers = () => {
 
     if (item_count.length != 0) {
       const el = parseInt($(".shopping-cart-items li:contains(" + name + ")").find(".item-quantity").find(".item-count").text());
-      // console.log(el)
       $(".shopping-cart-items li:contains(" + name + ")").find(".item-quantity").find(".item-count").text(el + 1);
     }
     else {
@@ -100,9 +22,8 @@ const addItemHandlers = () => {
         <span class="item-remove">X</span>
       </li>
     `);
-    // removeItemFromCart()
-
-  }
+      // removeItemFromCart()
+    }
   });
 }
 
@@ -137,10 +58,8 @@ const addEntree = (entree) => {
 const addToTotalOrders = () => {
   $('.add-to-order-top-container').click(function () {
     $(".shopping-cart").fadeIn("fast");
-    const fullList = $(".shopping-cart-items")
-    // console.log(fullList)
+    // const fullList = $(".shopping-cart-items")
     let totalOrders = parseInt($('.badge-outer').text());
-    // console.log(totalOrders)
     totalOrders += 1;
     $('.badge-outer').html(totalOrders);
     $('.badge-inner').html(totalOrders);
@@ -151,43 +70,25 @@ const addToOrderTotal = () => {
   $('.add-to-order-top-container').click(function (event) {
     const item = $(event.target);
     const food = item.parent().parent().parent().parent()
-    // const name = food.find(".gallery-title").text()
     const price = parseInt(food.find(".add-to-order-price").text().slice(1) * 100)
-    // console.log(price)
     let orderTotal = (($(".main-color-text").text().slice(1) * 100) + price) / 100
-    // console.log(orderTotal)
     $(".main-color-text").text("$" + orderTotal)
   })
 }
-// $(function () {
-  // console.log("it works!!")
-// const removeItemFromCart = () => {
-  $("body").on("click", ".item-remove", function (event) {
-    const item = $(event.target)
-    // console.log(target.length)
-    const target = $(this).parent()
-    // const dammmm = target.attr('data-name')
-    // console.log(dammmm)
-    // const id = item.attr('data-food-id')
-    // console.log($(this).parent())
-    const price = parseInt(target.find(".item-price").text().slice(1) * 100)
-    // console.log("price " + price)
-    const amount = parseInt(target.find(".item-quantity").find(".item-count").text())
-    // console.log("amount "+amount)
-    const currentTot = parseInt($(".main-color-text").text().slice(1) * 100)
-    // console.log("current tot "+currentTot)
-    const newtot = (currentTot - (amount * price)) / 100
-    // console.log("newtot  "+newtot)
-    $(".main-color-text").text("$" + newtot)
 
-    let totalOrders = parseInt($('.badge-outer').text());
-    // console.log(totalOrders)
-    totalOrders -= amount;
-    $('.badge-outer').html(totalOrders);
-    $('.badge-inner').html(totalOrders);
+$("body").on("click", ".item-remove", function (event) {
+  const item = $(event.target)
+  const target = $(this).parent()
+  const price = parseInt(target.find(".item-price").text().slice(1) * 100)
+  const amount = parseInt(target.find(".item-quantity").find(".item-count").text())
+  const currentTot = parseInt($(".main-color-text").text().slice(1) * 100)
+  const newtot = (currentTot - (amount * price)) / 100
 
-    $(this).parent().remove()
-  })
-// }
-// });
+  $(".main-color-text").text("$" + newtot)
+  let totalOrders = parseInt($('.badge-outer').text());
+  totalOrders -= amount;
+  $('.badge-outer').html(totalOrders);
+  $('.badge-inner').html(totalOrders);
 
+  $(this).parent().remove()
+})

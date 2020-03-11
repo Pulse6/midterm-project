5f33b854a5cf7ff691505a7d90bfc4386ce59e10
Shaheer Ahmed <shaheerahmeddev@gmail.com>
2020-03-11 04:07:30 +0000

    refactor server.js

--- server.js
+++ server.js
@@ -1,26 +1,85 @@
-// load .env data into process.env
+/* ————————————————————————— REQUIREMENTS ————————————————————————— */
+
+// LOAD DATA FROM .env INTO process.env \\
 require('dotenv').config();
 
-// Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
-const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
-const app        = express();
-const morgan     = require('morgan');
+// WEB SERVER CONFIGURATION \\
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
 
+// NODE MODULES \\
+const express       = require('express');
+const app           = express();
+const sass          = require('node-sass-middleware');
+const morgan        = require('morgan');
+const uuidv5        = require('uuid/v5');
+const bodyParser    = require('body-parser');
 const cookieSession = require('cookie-session');
-const uuidv5 = require('uuid/v5');
 
+// POSTGRES-SQL DATABASE CLIENT/CONNECTION SETUP \\
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
 
+/* ————————————————————————— MIDDLEWARE ————————————————————————— */
+
+// MORGAN (HTTP REQUEST LOGGER) \\
+/**
+ * Load the logger first so all (static) HTTP requests are logged to STDOUT.
+ * 'dev' = Concise output colored by response status for development use.
+ * The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+ **/
+app.use(morgan('dev'));
+
+// EJS (TEMPLATING ENGINE) \\
 app.set("view engine", "ejs");
+
+// REQ(UEST)/RES(PONSE) BODY PARSER \\
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// COOKIE SESSION \\
 app.use(cookieSession({
   name: 'session',
   keys: ['123']
 }));
 
-const generateRandomString = function() {/// pick 6 in total letter of number and return it to make a unique key
+// NODE-SASS (.scss COMPILER) \\
+app.use("/styles", sass({
+  src: __dirname + "/scss",
+  dest: __dirname + "/public/css",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+
+// SERVE ALL ASSETS OUT OF THE PUBLIC FOLDER \\
+app.use(express.static("public"));
+
+/* ————————————————————————— ROUTES / MOUNTING ————————————————————————— */
+
+/**
+ * Separated routes for each resource.
+ * NOTE: Feel free to replace the example routes below with your own.
+**/
+
+const usersRoutes = require("./routes/users");
+const menuRoutes = require("./routes/menu");
+const drinksRoutes = require("./routes/drinks");
+const orderRoutes = require("./routes/order");
+
+/**
+ * Mount all resource routes (i.e., the routes above).
+ * NOTE: Feel free to replace the example mounts below with your own.
+ * NOTE: Mount other resources here, using the same pattern above.
+**/
+
+app.use("/api/users", usersRoutes(db));
+app.use("/api/menu", menuRoutes(db));
+app.use("/api/drinks", drinksRoutes(db));
+app.use("/api/order", orderRoutes(db));
+
+/* ————————————————————————— HELPER FUNCTIONS ————————————————————————— */
+const generateRandomString = function() {
   let result = '';
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 6; i++) {
@@ -28,61 +87,6 @@ const generateRandomString = function() {/// pick 6 in total letter of number an
   }
   return result;
 };
-
-// PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
-
-
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
-
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/scss",
-  dest: __dirname + "/public/css",
-  debug: true,
-  outputStyle: 'expanded'
-}));
-app.use(express.static("public"));
-
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const orderRoutes = require("./routes/order");
-const menuRoutes = require("./routes/menu");
-const drinksRoutes = require("./routes/drinks");
-
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/order", orderRoutes(db));
-app.use("/api/menu", menuRoutes(db));
-app.use("/api/drinks", drinksRoutes(db));
-
-// Note: mount other resources here, using the same pattern above
-
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
-});
-
-app.get("/order", (req, res) => {
-  res.render("order.ejs");
-})
 
 const updateOrderTable = function(currentOrder) {
   console.log(currentOrder);
@@ -126,49 +130,41 @@ const updateOrderTable = function(currentOrder) {
     .then(res => res.rows[0]);
 };
 
-app.post("/api/order", (req, res) => {
-  const id = generateRandomString()
-  const urlUUID = uuidv5(id, uuidv5.URL);
-  req.session.userID = urlUUID;
+/* ————————————————————————— ROUTING ————————————————————————— */
 
-  const currentOrder = req.body.order;
-  console.log(Array.isArray(currentOrder));
-  console.log(typeof currentOrder)
+/**
+ *  Warning: avoid creating more routes in this file!
+ * Separate them into separate routes files (see above).'
+**/
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-  updateOrderTable(
-    {
-      owner_id: req.session.userID,
-      itemsInTheOrder: currentOrder
-    }
-  )
-  .then(cartOrder => {
-    res.send(cartOrder);
-    // res.redirect('/order');
-  })
-  .catch(e => {
-      console.error(e);
-      res.send(e);
-    });
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/order", (req, res) => {
+  res.render("order.ejs");
 })
 
+app.post("/api/order", (req, res) => {
+  const ID = generateRandomString()
+  const UUID = uuidv5(ID, uuidv5.URL);
 
-/* TESTINGTESTINGTESTINGTESTINGTESTINGTESTINGTESTING
+  req.session.userID = UUID;
+  const currentOrder = req.body.order;
 
-app.post('/order', (req, res) => {
-  const userID = req.session.userID;
-  const currentOrder = JSON.parse(localStorage.getItem('order'))
-  database.updateOrderTable({currentOrder, owner_id: userID})
-    .then(cartOrder => {
-      res.send(cartOrder);
-    })
+  updateOrderTable({owner_id: req.session.userID, itemsInTheOrder: currentOrder})
+    .then(cartOrder => { res.send(cartOrder); })
     .catch(e => {
-      console.error(e);
-      res.send(e);
-    });
+        console.error(e);
+        res.send(e);
+      });
+})
 
-TESTINGTESTINGTESTINGTESTINGTESTINGTESTINGTESTINGTESTING */
-
+/* ————————————————————————— SERVER LISTEN ————————————————————————— */
 app.listen(PORT, () => {
-  console.log(`The Second Breakfast Club listening on port: ${PORT}`);
+  console.log(`The Second Breakfast Club listening on port ${PORT}`);
 });

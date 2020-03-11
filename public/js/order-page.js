5691ea9a5ddb7d5bd9bd712ccc3d8599b44aa2f5
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-03-11 21:20:45 +0000

    add total summary to order.ejs

--- public/js/order-page.js
+++ public/js/order-page.js
@@ -30,61 +30,43 @@ const appendOrderSummaryItem = orderSummaryItem => {
   $(".all-cart-items").append(orderSummaryItemHTML);
 };
 
+const appendOrderSummaryItemTotal = orderSummaryItem => {
+  const orderSummaryItemTotalHTML = `
+  <li class="cart-list-item">
+        <div class="cart-item">
+          <div class="cart-item-description">
+            <span>
+              <p class="cart-item-label">Sub-Total:
+                <span class="cart-item-price">$${(orderSummaryItem / 100)}<span>
+              </p>
+              <p class="cart-item-label">Total:
+                <span class="cart-item-price">$${Math.round(orderSummaryItem / 100) * 1.13}<span>
+              </p>
+            </span>
+          </div>
+        </div>
+      </li>
+  `;
+
+  $(".all-cart-items").append(orderSummaryItemTotalHTML);
+};
 
 /* ————————————————————————— AJAX REQUESTS ————————————————————————— */
-
-//////////////////////////////// EXAMPLES FROM INDEX.EJS \/ \/ \/ \/ WILL CHANGE THEM ///////////////////////////////
-// GET MENU ITEMS FROM SQL DATABASE, SAVE TO LOCALSTORAGE, RENDER ON MAIN PAGE \\
-const getAndRenderMenu = () => {
-  $.ajax({
-    method: "GET",
-    url: "/api/menu"
-  }).done((res) => {
-    localStorage.setItem('food', JSON.stringify(res.menu));
-    for (food of res.menu) {
-      appendMenuItem(food);
-    }
-  });
-};
-
-// GET DRINK ITEMS FROM SQL DATABASE, SAVE TO LOCALSTORAGE, RENDER ON MAIN PAGE \\
-const getAndRenderDrinks = () => {
-  $.ajax({
-    method: "GET",
-    url: "/api/drinks"
-  }).done((res) => {
-    localStorage.setItem('drinks', JSON.stringify(res.drinks));
-    for (drink of res.drinks) {
-      appendMenuItem(drink);
-    }
-  });
-};
-
-// POST CART ORDER TO /api/order & REDIRECT TO /order \\
-const postOrderAndRedirect = () => {
-  $("body").on("click", ".button", function(event) {
-    event.preventDefault();
-    let order = JSON.parse(localStorage.getItem('order'));
-    $.ajax({
-      method: "POST",
-      url: "/api/order",
-      data: { order }
-    }).done((res) => {
-      window.location.replace('/order');
-    })
-  });
-};
-
 
 const getAndRenderOrderSummaryItems = () => {
   $.ajax({
     method: "GET",
     url: "/api/order"
   }).done((res) => {
-    // console.log(res.orders)
     for (orderItem of res.orders) {
       appendOrderSummaryItem(orderItem);
     }
+
+    let totalPrice = 0;
+    for (orderItem of res.orders) {
+      totalPrice += orderItem.item_price;
+    }
+    appendOrderSummaryItemTotal(totalPrice)
   });
 };
 
@@ -94,6 +76,5 @@ const getAndRenderOrderSummaryItems = () => {
 
 $(function() {
   getAndRenderOrderSummaryItems();
-  // appendOrderSummaryItem();
   preventOrderButtonRefresh();
 });

4c52e42dde2ff97e9ecd3e5cacfc80220b54e5d5
Shaheer Ahmed <shaheerahmeddev@gmail.com>
2020-03-11 04:33:46 +0000

    move image files from public > assets > images to public > assets > img, update corresponding seed data

--- db/seeds/02_menu_seeds.sql
+++ db/seeds/02_menu_seeds.sql
@@ -3,14 +3,14 @@ INSERT INTO menu (name, description, food_category, item_img, price) VALUES
     'Fish -N- Chips',
     'The best fried halibut you''ll ever have. Made to order daily by S.Gamgee himself, you can''t say no to that!',
     'entrees',
-    '/images/fish-and-chips.jpg',
+    'assets/img/food/fish-and-chips.jpg',
     1999
   ),
   (
     'Beef Wellington',
     'Beef fillet, surrounded in love and baked to perfection in Fatty Bolger approved puff pastry. This''ll be sure to put a smile on your Old Gaffers face.',
     'entrees',
-    '/images/BeefWellington.jpg',
+    'assets/img/food/BeefWellington.jpg',
     2999
 
   ),
@@ -18,63 +18,63 @@ INSERT INTO menu (name, description, food_category, item_img, price) VALUES
     'Shepherd Pie',
     'From the bounty of the fields we''ve all ''borrowed'' from, but this time with his approval: Farmer Maggot Shepherd''s pie will not dissapoint.',
     'entrees',
-    '/images/shepherds-pie.jpg',
+    'assets/img/food/shepherds-pie.jpg',
     1499
   ),
   (
     'Chef Curry',
     'While we Hobbits don''t normally hold well with foreign food this spicy curry from the East came to us back when Old Bilbo turned up not dead. Now we still don''t reckon much for travelling see, but this here is a mighty fine dish that many a Gaffer has taken too and you should too!',
     'entrees',
-    '/images/rustic-curry.jpg',
+    'assets/img/food/rustic-curry.jpg',
     1499
   ),
   (
     'The Prime Rib',
     'From the pastures of Tookland comes a feast that is every Hobbit''s favourite meal. Prime rib served with creamy mashed potatoes, mixed vegetables, gravy and Shire puddings.',
     'entrees',
-    '/images/prime-rib.jpeg',
+    'assets/img/food/prime-rib.jpeg',
     2499
   ),
   (
     'Sauteed Mushrooms',
     'A far safer shortcut to mushrooms than you''re used to and our number #1 bestseller. A collection of wild mushrooms and mushrooms brough to us by Farmer Maggot this dish speaks for itself.',
     'appetizers',
-    '/images/mushrooms.jpg',
+    'assets/img/food/mushrooms.jpg',
     999
   ),
   (
     'Fresh Chips',
     'Brought to you by our guest chef S.Gamgee these are the best damned chips you''ll ever have.',
     'appetizers',
-    '/images/french-fries.jpeg',
+    'assets/img/food/french-fries.jpeg',
     499
   ),
   (
     'French Onion Soup',
     'We''re not sure where French comes from or what exactly that is but this onion soup''s name is as old as our love for it. With our broth including vintages from the wine-cellars of Brandybuck Hall this soup has quickly become a local favourite',
     'appetizers',
-    '/images/french-onion-soup.jpg',
+    'assets/img/food/french-onion-soup.jpg',
     999
   ),
   (
     'Chocolate Gateau',
     'Dark, decadent and as light as you can imagine. Some say Hobbits are the best bakers this side of the Brandywine, but they''d be wrong...we''re the best bakers in Middle Earth',
     'desserts',
-    '/images/chocolate-gateau.jpeg',
+    'assets/img/food/chocolate-gateau.jpeg',
     799
   ),
   (
     'Strawberries and Cream',
     'From the orchards and dairies of the Cotton''s this dish is sweet, light and as lovely as Rossie Cotton herself',
     'desserts',
-    '/images/strawberries-cream.jpg',
+    'assets/img/food/strawberries-cream.jpg',
     599
   ),
   (
     'Butter Tarts',
     'These are the best tarts around. Enough said. just order it alright man',
     'desserts',
-    '/images/butter-tarts.jpg',
+    'assets/img/food/butter-tarts.jpg',
     599
   );
 

581ee60a5b800c0e64cd977654117a72ee7482d5
Sebastian Lynch <lynchseb@gmail.com>
2020-03-12 16:52:40 +0000

    fix drinks bug not showing imgs on order page

--- db/seeds/04_drink_seeds.sql
+++ db/seeds/04_drink_seeds.sql
@@ -3,27 +3,27 @@ INSERT INTO drinks (name, description, food_category, item_img, price) VALUES
     'Dark Stoute',
     'Smooth and creamy this Stoute is the pride of the Brandybucks and we''re proud to serve it to you',
     'drinks',
-    '/assets/img/drinks/dark-stout.jpg',
+    'assets/img/drinks/dark-stout.jpg',
     599
   ),
   (
     'Hobbit Ale',
     'The most passable ale you''ll find. Goes down easy, is light on the pocket and there''s always plenty more',
     'drinks',
-    '/assets/img/drinks/hobbit-ale.jpg',
+    'assets/img/drinks/hobbit-ale.jpg',
     399
   ),
   (
     'Second Breakfast Sour',
     'Of our own design and exclusive to Second Breakfast, you won''t sour on this twist on our own Hobbit''s Ale',
     'drinks',
-    '/assets/img/drinks/sourbeer.jpg',
+    'assets/img/drinks/sourbeer.jpg',
     499
   ),
   (
     'Juices',
     'A variety of locally sourced beverages suitable for your younglings',
     'drinks',
-    '/assets/img/drinks/fruit-juice.jpeg',
+    'assets/img/drinks/fruit-juice.jpeg',
     299
   );
